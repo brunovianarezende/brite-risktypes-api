b304c9761f8727db34210766a2820cfe54b2d706
Bruno Rezende <brunovianarezende@gmail.com>
2017-12-05 13:54:55 -0200

    Improve the way instances are modelled
    now it is allowed for sub-types to have instance restrictions

--- data/brite/model/__init__.py
+++ data/brite/model/__init__.py
@@ -12,24 +12,59 @@ class RiskType(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     description = Column(String)
-    attributes = relationship("RiskTypeAttribute", back_populates="risk_type")
+    attributes = relationship("AttributeType", back_populates="risk_type")
     instances = relationship("RiskTypeInstance", back_populates="risk_type")
 
-class RiskTypeAttributeDataType(enum.Enum):
+class AttributeDataType(enum.Enum):
     INT = 1
     TEXT = 2
     DATE = 3
     ENUM = 4
 
-class RiskTypeAttribute(Base):
+class AttributeType(Base):
     __tablename__ = 'risk_type_attribute'
 
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
-    data_type = Column(Enum(RiskTypeAttributeDataType), nullable=False)
+    description = Column(String)
+    data_type = Column(Enum(AttributeDataType), nullable=False)
     risk_type_id = Column(Integer, ForeignKey('risk_type.id'), nullable=False)
     risk_type = relationship("RiskType", back_populates="attributes")
-    instances = relationship("RiskTypeAttributeInstance", back_populates="attribute_type")
+    instances = relationship("AttributeInstance", back_populates="attribute_type")
+
+    __mapper_args__ = {
+        'polymorphic_on': data_type,
+    }
+
+class AttributeTypeInt(AttributeType):
+    __mapper_args__ = {
+        'polymorphic_identity': AttributeDataType.INT
+    }
+
+class AttributeTypeText(AttributeType):
+    __mapper_args__ = {
+        'polymorphic_identity': AttributeDataType.TEXT
+    }
+
+class AttributeTypeDate(AttributeType):
+    __mapper_args__ = {
+        'polymorphic_identity': AttributeDataType.DATE
+    }
+
+class AttributeTypeEnum(AttributeType):
+    allowed_values = relationship("AttributeTypeEnumValue", back_populates="enum_type")
+
+    __mapper_args__ = {
+        'polymorphic_identity': AttributeDataType.ENUM
+    }
+
+class AttributeTypeEnumValue(Base):
+    __tablename__ = 'risk_type_attribute_enum_value'
+
+    id = Column(Integer, primary_key=True)
+    value = Column(String, nullable=False)
+    enum_type_id = Column(Integer, ForeignKey('risk_type_attribute.id'), nullable=False)
+    enum_type = relationship("AttributeTypeEnum", back_populates="allowed_values")
 
 class RiskTypeInstance(Base):
     __tablename__ = 'risk_type_instance'
@@ -37,14 +72,46 @@ class RiskTypeInstance(Base):
     id = Column(Integer, primary_key=True)
     risk_type_id = Column(Integer, ForeignKey('risk_type.id'), nullable=False)
     risk_type = relationship("RiskType", back_populates="instances")
-    attributes_instances = relationship("RiskTypeAttributeInstance", back_populates="risk_type_instance")
+    attributes_instances = relationship("AttributeInstance", back_populates="risk_type_instance")
 
-class RiskTypeAttributeInstance(Base):
+class AttributeInstance(Base):
     __tablename__ = 'risk_type_attribute_instance'
 
     id = Column(Integer, primary_key=True)
+    attribute_data_type = Column(Enum(AttributeDataType), nullable=False)
     attribute_type_id = Column(Integer, ForeignKey('risk_type_attribute.id'), nullable=False)
-    attribute_type = relationship("RiskTypeAttribute", back_populates="instances")
+    attribute_type = relationship("AttributeType", back_populates="instances")
     risk_type_instance_id = Column(Integer, ForeignKey('risk_type_instance.id'), nullable=False)
     risk_type_instance = relationship("RiskTypeInstance", back_populates="attributes_instances")
-    value = Column(Integer)
+
+    __mapper_args__ = {
+        'polymorphic_on': attribute_data_type,
+    }
+
+class AttributeInstanceInt(AttributeInstance):
+    int_value = Column(Integer)
+
+    __mapper_args__ = {
+        'polymorphic_identity': AttributeDataType.INT
+    }
+
+class AttributeInstanceText(AttributeInstance):
+    text_value = Column(Integer)
+
+    __mapper_args__ = {
+        'polymorphic_identity': AttributeDataType.TEXT
+    }
+
+class AttributeInstanceDate(AttributeInstance):
+    date_value = Column(Integer)
+
+    __mapper_args__ = {
+        'polymorphic_identity': AttributeDataType.DATE
+    }
+
+class AttributeInstanceEnum(AttributeInstance):
+    enum_value = Column(String)
+
+    __mapper_args__ = {
+        'polymorphic_identity': AttributeDataType.ENUM
+    }
--- data/tests/model/test_model.py
+++ data/tests/model/test_model.py
@@ -1,14 +1,17 @@
 import unittest
+import datetime
 
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-from brite.model import Base, RiskType, RiskTypeAttribute, RiskTypeAttributeDataType,\
-    RiskTypeInstance, RiskTypeAttributeInstance
+from brite.model import Base, RiskType, AttributeType, AttributeDataType,\
+    AttributeTypeInt, AttributeTypeText, AttributeTypeDate, AttributeTypeEnum,\
+    AttributeTypeEnumValue, RiskTypeInstance, AttributeInstanceInt, AttributeInstanceText,\
+    AttributeInstanceDate, AttributeInstanceEnum
 
 class TestRawRiskTypeModel(unittest.TestCase):
     def setUp(self):
-        # make sure we always start from scratch
+        # make sure each test starts with a clean database
         engine = create_engine('sqlite:///:memory:', echo=True)
         Base.metadata.create_all(engine)
         self.Session = sessionmaker(bind=engine)
@@ -26,13 +29,62 @@ class TestRawRiskTypeModel(unittest.TestCase):
         session = self.Session()
         risk_type = RiskType(name='Golf prize')
         session.add(risk_type)
-        prize_value = RiskTypeAttribute(name='Value', data_type=RiskTypeAttributeDataType.INT, risk_type=risk_type)
+        prize_value = AttributeType(name='Value', data_type=AttributeDataType.INT, risk_type=risk_type)
         session.add(prize_value)
         session.commit()
         found_risk_type = session.query(RiskType).filter_by(name='Golf prize').first()
-        found_risk_type_attribute = session.query(RiskTypeAttribute).filter_by(name='Value').first()
+        found_risk_type_attribute = session.query(AttributeType).filter_by(name='Value').first()
         assert found_risk_type_attribute.risk_type == risk_type
         assert found_risk_type.attributes == [prize_value]
+
+    def test_add_new_risk_type_attribute_Int(self):
+        session = self.Session()
+        risk_type = RiskType(name='Golf prize')
+        session.add(risk_type)
+        prize_value = AttributeTypeInt(name='Value', risk_type=risk_type)
+        session.add(prize_value)
+        session.commit()
+        found_risk_type = session.query(RiskType).filter_by(name='Golf prize').first()
+        found_risk_type_attribute = session.query(AttributeType).filter_by(name='Value').first()
+        assert found_risk_type_attribute.risk_type == risk_type
+        assert found_risk_type.attributes == [prize_value]
+
+    def test_add_new_risk_type_attribute_Text(self):
+        session = self.Session()
+        risk_type = RiskType(name='Golf prize')
+        session.add(risk_type)
+        attr_type = AttributeTypeText(name='Player', risk_type=risk_type)
+        session.add(attr_type)
+        session.commit()
+        found_risk_type = session.query(RiskType).filter_by(name='Golf prize').first()
+        found_risk_type_attribute = session.query(AttributeType).filter_by(name='Player').first()
+        assert found_risk_type_attribute.risk_type == risk_type
+        assert found_risk_type.attributes == [attr_type]
+
+    def test_add_new_risk_type_attribute_Date(self):
+        session = self.Session()
+        risk_type = RiskType(name='Golf prize')
+        session.add(risk_type)
+        attr_type = AttributeTypeDate(name='When', risk_type=risk_type)
+        session.add(attr_type)
+        session.commit()
+        found_risk_type = session.query(RiskType).filter_by(name='Golf prize').first()
+        found_risk_type_attribute = session.query(AttributeType).filter_by(name='When').first()
+        assert found_risk_type_attribute.risk_type == risk_type
+        assert found_risk_type.attributes == [attr_type]
+
+    def test_add_new_risk_type_attribute_Enum(self):
+        session = self.Session()
+        risk_type = RiskType(name='Golf prize')
+        session.add(risk_type)
+        allowed_values = [AttributeTypeEnumValue(value=v) for v in ['EASY', 'NORMAL', 'HARD']]
+        attr_type = AttributeTypeEnum(name='level', risk_type=risk_type, allowed_values=allowed_values)
+        session.add(attr_type)
+        session.commit()
+        found_risk_type = session.query(RiskType).filter_by(name='Golf prize').first()
+        found_risk_type_attribute = session.query(AttributeType).filter_by(name='level').first()
+        assert found_risk_type_attribute.risk_type == risk_type
+        assert found_risk_type.attributes == [attr_type]
 
     def test_add_new_risk_type_instance(self):
         session = self.Session()
@@ -44,17 +96,80 @@ class TestRawRiskTypeModel(unittest.TestCase):
         assert risk_type_instance == found_risk_type_instance
         assert found_risk_type_instance.id is not None
 
-    def test_add_new_risk_type_attribute_instance(self):
+    def test_add_new_risk_type_attribute_instance_IntAttribute(self):
         session = self.Session()
         risk_type = RiskType(name='Golf prize')
         session.add(risk_type)
-        prize_value = RiskTypeAttribute(name='Value', data_type=RiskTypeAttributeDataType.INT, risk_type=risk_type)
-        session.add(prize_value)
+        attr_type = AttributeType(name='Value', data_type=AttributeDataType.INT, risk_type=risk_type)
+        session.add(attr_type)
 
         risk_type_instance = RiskTypeInstance(risk_type=risk_type)
         session.add(risk_type_instance)
 
-        risk_type_attribute_instance = RiskTypeAttributeInstance(risk_type_instance=risk_type_instance, attribute_type=prize_value, value=10)
+        risk_type_attribute_instance = AttributeInstanceInt(risk_type_instance=risk_type_instance,
+                                            attribute_type=attr_type, int_value=10)
+        session.add(risk_type_attribute_instance)
+
+        session.commit()
+
+        found_risk_type_instance = session.query(RiskTypeInstance).filter_by(risk_type=risk_type).first()
+
+        assert found_risk_type_instance.attributes_instances == [risk_type_attribute_instance]
+
+    def test_add_new_risk_type_attribute_instance_TextAttribute(self):
+        session = self.Session()
+        risk_type = RiskType(name='Golf prize')
+        session.add(risk_type)
+        attr_type = AttributeTypeText(name='Player', risk_type=risk_type)
+        session.add(attr_type)
+
+        risk_type_instance = RiskTypeInstance(risk_type=risk_type)
+        session.add(risk_type_instance)
+
+        risk_type_attribute_instance = AttributeInstanceText(risk_type_instance=risk_type_instance,
+                                            attribute_type=attr_type, text_value='John')
+        session.add(risk_type_attribute_instance)
+
+        session.commit()
+
+        found_risk_type_instance = session.query(RiskTypeInstance).filter_by(risk_type=risk_type).first()
+
+        assert found_risk_type_instance.attributes_instances == [risk_type_attribute_instance]
+
+    def test_add_new_risk_type_attribute_instance_DateAttribute(self):
+        session = self.Session()
+        risk_type = RiskType(name='Golf prize')
+        session.add(risk_type)
+        attr_type = AttributeTypeDate(name='When', risk_type=risk_type)
+        session.add(attr_type)
+
+        risk_type_instance = RiskTypeInstance(risk_type=risk_type)
+        session.add(risk_type_instance)
+
+        risk_type_attribute_instance = AttributeInstanceDate(risk_type_instance=risk_type_instance,
+                                            attribute_type=attr_type, date_value=datetime.date(2017, 12, 15))
+        session.add(risk_type_attribute_instance)
+
+        session.commit()
+
+        found_risk_type_instance = session.query(RiskTypeInstance).filter_by(risk_type=risk_type).first()
+
+        assert found_risk_type_instance.attributes_instances == [risk_type_attribute_instance]
+
+    def test_add_new_risk_type_attribute_instance_EnumAttribute(self):
+        session = self.Session()
+        risk_type = RiskType(name='Golf prize')
+        session.add(risk_type)
+
+        allowed_values = [AttributeTypeEnumValue(value=v) for v in ['EASY', 'NORMAL', 'HARD']]
+        attr_type = AttributeTypeEnum(name='Level', risk_type=risk_type, allowed_values=allowed_values)
+        session.add(attr_type)
+
+        risk_type_instance = RiskTypeInstance(risk_type=risk_type)
+        session.add(risk_type_instance)
+
+        risk_type_attribute_instance = AttributeInstanceEnum(risk_type_instance=risk_type_instance,
+                                            attribute_type=attr_type, enum_value='EASY')
         session.add(risk_type_attribute_instance)
 
         session.commit()
